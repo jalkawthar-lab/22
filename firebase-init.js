46279e04dee73f5d59bdfcfdb6d9fbd8a4f9429f
jalkawthar-lab <j.alkawthar@gmail.com>
2026-06-25 00:33:51 +0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,34 +1,50 @@
-<script type="module">
-  // 1. استدعاء التطبيق الأساسي
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  
-  // 2. استدعاء دوال قاعدة البيانات (Firestore) الضرورية لعمل المتجر
-  import { 
-    getFirestore, doc, collection, getDoc, getDocs, addDoc, updateDoc, deleteDoc, setDoc, serverTimestamp, onSnapshot 
-  } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
-  
-  // إعدادات الربط الخاصة بك
-  const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+// قمنا هنا باستدعاء جميع أدوات قاعدة البيانات (Firestore) اللازمة للعمل
+import { 
+    getFirestore, 
+    collection, 
+    getDocs, 
+    getDoc, 
+    addDoc, 
+    setDoc, 
+    updateDoc, 
+    deleteDoc, 
+    doc, 
+    onSnapshot, 
+    serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+// إعدادات مشروعك الجديد (etsyShopIraq)
+const firebaseConfig = {
     apiKey: "AIzaSyBZ4kbwr42QtMi3ujHwRZvttaTAjNUu6Kw",
     authDomain: "ioiu-8b57a.firebaseapp.com",
     projectId: "ioiu-8b57a",
     storageBucket: "ioiu-8b57a.firebasestorage.app",
     messagingSenderId: "128338798689",
     appId: "1:128338798689:web:fbc5c7dd14291660ba6220"
-  };
+};
 
-  // تهيئة فايربيس
-  const app = initializeApp(firebaseConfig);
-  
-  // تهيئة قاعدة البيانات
-  const db = getFirestore(app);
+// تهيئة التطبيق
+const app = initializeApp(firebaseConfig);
 
-  // تعريف المتغيرات التي ينتظرها ملف script.js لكي يعمل
-  window.db = db;
-  window.firestore = {
-    doc, collection, getDoc, getDocs, addDoc, updateDoc, deleteDoc, setDoc, serverTimestamp, onSnapshot
-  };
+// الاتصال بقاعدة البيانات
+const db = getFirestore(app);
 
-  // إرسال إشعار للمتجر والآدمن بأن قاعدة البيانات أصبحت جاهزة
-  window.dispatchEvent(new Event("firebaseReady"));
-</script>
+// هذه الخطوة مهمة جداً: نجعل أدوات قاعدة البيانات متاحة لملف script.js لكي يستخدمها
+window.db = db;
+window.firestore = {
+    collection,
+    getDocs,
+    getDoc,
+    addDoc,
+    setDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+    onSnapshot,
+    serverTimestamp
+};
+
+// إطلاق إشارة لباقي ملفات المشروع أن الاتصال بقاعدة البيانات جاهز ويمكنهم البدء
+const event = new Event('firebaseReady');
+window.dispatchEvent(event);
