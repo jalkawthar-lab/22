43ff1e52cf502716cd3679565e39949b28b0a363
etsyShopIraq <j.alkawthar@gmail.com>
2026-06-28 18:32:35 +0300

    Clean up comments in firebase-init.js
    Removed comments for clarity and improved code readability.

--- firebase-init.js
+++ firebase-init.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-// Here we have imported all the necessary database (Firestore) tools required for operation
 import { 
     getFirestore, 
     collection, 
@@ -14,7 +13,6 @@ import {
     serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-// Settings for your new project (etsyShopIraq)
 const firebaseConfig = {
     apiKey: "AIzaSyBZ4kbwr42QtMi3ujHwRZvttaTAjNUu6Kw",
     authDomain: "ioiu-8b57a.firebaseapp.com",
@@ -24,13 +22,9 @@ const firebaseConfig = {
     appId: "1:128338798689:web:fbc5c7dd14291660ba6220"
 };
 
-// Initialize the application
 const app = initializeApp(firebaseConfig);
-
-// Connect to the database
 const db = getFirestore(app);
 
-// This step is very important: we make the database tools available to the script.js file for its use
 window.db = db;
 window.firestore = {
     collection,
@@ -45,6 +39,5 @@ window.firestore = {
     serverTimestamp
 };
 
-// Trigger a signal to the rest of the project files that the database connection is ready and they can begin
 const event = new Event('firebaseReady');
 window.dispatchEvent(event);
